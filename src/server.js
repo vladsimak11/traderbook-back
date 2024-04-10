b5792ddd8d90efbcda5f5bf1d6ce9bbe7e295008
Vladyslav Simak <simakv21@gmail.com>
2024-04-10 11:49:57 +0300

    change port

--- src/server.js
+++ src/server.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const { DB_HOST, PORT = 3000} = process.env;
+const { DB_HOST, PORT = 3001} = process.env;
 
 mongoose.set('strictQuery', true);
 
 mongoose.connect(DB_HOST)
   .then(() => {
     app.listen(PORT, () => {
-      console.log("Database connection successful!")
+      console.log(`Database connection successful! Port: ${PORT}`)
     });
   })
   .catch( error => {
