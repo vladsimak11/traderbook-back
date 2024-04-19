40ccdf1f4a4922aff4a4ab02dbac57de9974d715
Vladyslav Simak <simakv21@gmail.com>
2024-04-19 14:53:53 +0300

    delete token

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -26,7 +26,6 @@ const register = async (req, res, next) => {
       ...req.body,
       password: hashPassword,
       verificationToken,
-      token: null,
     });
 
     const verifyEmail = {
@@ -36,15 +35,6 @@ const register = async (req, res, next) => {
     };
 
     await sendEmail(verifyEmail);
-
-    const payload = {
-      id: newUser._id,
-    };
-
-    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
-
-    newUser.token = token;
-    await newUser.save();
 
     res.status(201).json({
       user: {
